f4551037d32815e25ac332f916e4725a52c4bd76
ahmednule <nuleahmed6@gmail.com>
2026-02-13 21:39:18 +0300

    feature: learned about arrays in python

---
+++ index.py
@@ -0,0 +1,17 @@
+# Arrays in python which can also be called lists
+
+arr = [1, 2, 3, 4, 5]
+
+# arr.append(6)
+# arr.append(100)
+# arr.pop()
+
+# arr.pop(0)
+# arr.remove(3)
+
+for i in range(len(arr)):
+    print(arr[i]*2)
+
+# print(arr)
+
+#2
